e9b7ba1c7f91e6f4377b511de02dce54fe52ff23
plgkiet <plgkiet.work@gmail.com>
2024-12-20 01:43:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -36,3 +36,19 @@ function generateWish() {
 }
 
 generateWish();
+
+
+document.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+});
+
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'F12' ||
+        (e.ctrlKey && e.shiftKey && e.key === 'I') ||
+        (e.ctrlKey && e.key === 'U')) {
+        e.preventDefault();
+        
+    }
+});
+
